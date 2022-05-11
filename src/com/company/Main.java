2134bb3d0c02acddba82f1ad53b8189c0d640856
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-05-11 12:24:08 +0200

    test push Swing

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Test");
         new Main().run();
         //Engine engine = new Engine();
         //engine.execute();
