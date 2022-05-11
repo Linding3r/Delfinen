f026813c999e8799d71e8eeeabf31248e7418b4f
morten-l-k <mort75q3@stud.kea.dk>
2022-05-11 10:40:15 +0200

    Added MemberList class

---
+++ src/com/company/MemberList.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class MemberList {
+  private ArrayList<Member> memberList = new ArrayList<>();
+
+  public void addMemberToList(Member member){
+    memberList.add(member);
+  }
+
+}
