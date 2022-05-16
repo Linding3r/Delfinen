224801e037612ed00889b3def2dace3f28fa7258
Frederik Hallengreen <fred70p9@stud.kea.dk>
2022-05-16 13:39:26 +0200

    changes to Birthday

--- src/com/company/Birthday.java
+++ src/com/company/Birthday.java
@@ -1,17 +1,22 @@
-package com.company;
+package main;
 
+import java.time.LocalDate;
 import java.time.Year;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Birthday {
-
     private int day;
     private int month;
     private int year;
+    private int age;
     Scanner sc = new Scanner(System.in);
+    private String birthday = sc.nextLine();
+    private LocalDate currentDate = LocalDate.now();
 
-    public void checkBirthday(String birthday) {
+
+
+    public String checkBirthday() {
         String[] birthdayArray = birthday.split("/");
         try {
             for (int i = 0; i < birthdayArray.length; i++) {
@@ -19,24 +24,47 @@ public class Birthday {
                 month = Integer.parseInt(birthdayArray[1]);
                 year = Integer.parseInt(birthdayArray[2]);
             }
-        } catch (ArrayIndexOutOfBoundsException ignored) {
+        } catch (ArrayIndexOutOfBoundsException  | NumberFormatException ignored) {
         }
-        while (!(day >= 1 && day <= 31 && month >= 1 && month <= 12 && year >= 1900 && year <= Integer.parseInt(String.valueOf(Year.now())))) {
-            System.out.println((Arrays.toString(birthdayArray).replace("{","").replace("}","")) + " Er en ugyldig fødselsdagsdato. Indtast ny dato:");
+        while (!(day >= 1 && day <= 31 && month >= 1 && month <= 12 && year >= 1900
+                && year <= Integer.parseInt(String.valueOf(Year.now())))) {
+            System.out.println((Arrays.toString(birthdayArray).replace("{","").replace("}","")) +
+                    " Er en ugyldig fødselsdagsdato. Indtast ny dato:");
             birthday = sc.nextLine();
-            checkBirthday(birthday);
+            checkBirthday();
         }
+        return birthday;
     }
 
-    public int getDay() {
-        return day;
+    public String getBirthday() {
+        return birthday;
     }
 
-    public int getMonth() {
-        return month;
+    public int birthdayToAge(){
+        int ageYear = currentDate.getYear() - year;
+        int ageMonth = currentDate.getMonthValue() - month;
+        int ageDay = currentDate.getDayOfMonth() - day;
+
+        if(ageMonth < 0 || ageDay < 0) {
+            if (ageMonth < 0) {
+                ageYear--;
+            } if(ageDay < 0){
+                ageMonth--;
+                if(ageMonth < 0) {
+                    ageYear--;
+
+                }
+            }
+        } return age = ageYear;
     }
 
-    public int getYear() {
-        return year;
+    public String membershipType(){
+        if(age < 18){
+            return "Junior";
+        } else if (age >= 18 && age < 65){
+            return "Senior";
+        }else return "65+";
     }
+
+
 }
