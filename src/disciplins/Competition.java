b1f9babb73682cab9486a01a1e952bde611625d6
KaptajnKirk <theodor@duus-kirk.dk>
2022-05-20 13:58:07 +0200

    Added competiton class, competiton saves to/loads from CSV method

---
+++ src/disciplins/Competition.java
@@ -0,0 +1,33 @@
+package disciplins;
+
+public class Competition {
+
+  private SwimmingTime swimmingTime;
+  private String competitionName;
+  private int memberId;
+  private String placement;
+
+  public Competition (SwimmingTime swimmingTime, String competition, int memberId, String placement){
+    this.swimmingTime = swimmingTime;
+    this.competitionName = competition;
+    this.memberId = memberId;
+    this.placement = placement;
+  }
+
+  public SwimmingTime getSwimmingTime() {
+    return swimmingTime;
+  }
+
+  public String getCompetitionName() {
+    return competitionName;
+  }
+
+  public int getMemberId() {
+    return memberId;
+  }
+
+  public String getPlacement() {
+    return placement;
+  }
+
+}
